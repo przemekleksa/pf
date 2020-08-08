daba627d8085d6c7d285169670fc0971a5833dfc
Przemyslaw Leksa <38156110+gnoosny@users.noreply.github.com>
2020-08-08 16:21:44 +0200

    pf

---
+++ precache-manifest.b9b0d18eb95d7471e893c26c82fd7246.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94b3bd1d7e8ec7ea6cf032c293d7ed92",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c46a62cd357b264ac993",
+    "url": "/static/css/main.2a19b613.chunk.css"
+  },
+  {
+    "revision": "3777a71ade47bf557767",
+    "url": "/static/js/2.5dcfb8f1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5dcfb8f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c46a62cd357b264ac993",
+    "url": "/static/js/main.a13a7885.chunk.js"
+  },
+  {
+    "revision": "e3473a344493b6ab086d",
+    "url": "/static/js/runtime-main.21e4bbbf.js"
+  },
+  {
+    "revision": "e054e0d80d6073db7ba08b24c7261a96",
+    "url": "/static/media/Przemyslaw_Leksa_Resume.e054e0d8.pdf"
+  },
+  {
+    "revision": "fd309efee1ce8de0ea897306bfd22cd5",
+    "url": "/static/media/atmSingle.fd309efe.jpg"
+  },
+  {
+    "revision": "b69a804086ee5731e331e461ce6a2793",
+    "url": "/static/media/chatSingle.b69a8040.jpg"
+  },
+  {
+    "revision": "95b899c74eccd64040c5e3746a560b77",
+    "url": "/static/media/currencySingle.95b899c7.jpg"
+  },
+  {
+    "revision": "a0322a747c2254aa148c590fe9dcc410",
+    "url": "/static/media/drip.a0322a74.jpg"
+  },
+  {
+    "revision": "98131c7aac124703e04b3361944bad36",
+    "url": "/static/media/keyboardSingle.98131c7a.jpg"
+  },
+  {
+    "revision": "30ac7139e1ee342ecce9ffc3656cbe79",
+    "url": "/static/media/neutronSingle.30ac7139.jpg"
+  },
+  {
+    "revision": "1bde85610c19792714f160cce871d280",
+    "url": "/static/media/pizzaSingle.1bde8561.jpg"
+  },
+  {
+    "revision": "b070d836c7b99061253a900a1043bd98",
+    "url": "/static/media/przemek.b070d836.png"
+  },
+  {
+    "revision": "351233b161bad175879b4b86c3ecfc8f",
+    "url": "/static/media/przemekleksacv.351233b1.jpg"
+  },
+  {
+    "revision": "a739b6a4d5133aa393b48071641ada3a",
+    "url": "/static/media/restaurantSingle.a739b6a4.jpg"
+  },
+  {
+    "revision": "c8202d9dee511d3a74c6b3742f9a305d",
+    "url": "/static/media/sassSingle.c8202d9d.jpg"
+  },
+  {
+    "revision": "fe550e04846e619ad10729ece3c44584",
+    "url": "/static/media/turismSingle.fe550e04.jpg"
+  },
+  {
+    "revision": "be97ace661228331560873b3d0b2fb8c",
+    "url": "/static/media/weatherSingle.be97ace6.jpg"
+  }
+]);
